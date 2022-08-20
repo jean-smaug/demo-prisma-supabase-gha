62066d6ac35ca63b261341b3a114cb8b5087cd39
Maxime BLANC <maxime.blanc@bedrockstreaming.com>
2022-08-20 21:03:59 +0200

    with seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,12 +1,12 @@
 import { PrismaClient } from "@prisma/client";
-import * as crypto from "node:crypto";
 
 const prisma = new PrismaClient();
 
 (async () => {
   prisma.post.create({
     data: {
-      id: crypto.randomUUID(),
+      id: 1,
+      title: "Salut smaug",
     },
   });
 })();
